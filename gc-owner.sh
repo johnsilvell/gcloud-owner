ddd7a3de7a0aaee9f60652512e4c06eba9cbe7fc
John Silvell (BN) <john.silvell@bonniernews.se>
2025-11-28 15:40:02 +0100

    Added support to store data in txt-files

--- gc-owner.sh
+++ gc-owner.sh
@@ -8,11 +8,17 @@ if [ -z "$1" ]; then
 fi
 
 PROJECT_ID=$1
+echo
 echo "Setting project to: $PROJECT_ID"
 
 ROLE_TO_FIND="roles/owner"
 
+# Get the current timestamp
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+
+
 echo "Searching for members with the '$ROLE_TO_FIND' role in project '$PROJECT_ID'..."
+echo
 echo "-----------------------------------------------"
 
 # 2. Fetch the IAM policy for the specified project
@@ -23,4 +29,42 @@ gcloud projects get-iam-policy "$PROJECT_ID" \
     --flatten="bindings[].members" \
     --filter="bindings.role:$ROLE_TO_FIND" \
     --format="csv[no-heading](bindings.members)"
+echo "-----------------------------------------------"
+echo 
+
+# 4. Save reslults to a file
+read -r -p "Do you want to save the results to a file? (y/n): " save_to_file
+echo
+
+# Convert answer to lowercase
+save_to_file=$(echo "$save_to_file" | tr '[:upper:]' '[:lower:]')
+
+if [[ "$save_to_file" == "y" || "$save_to_file" == "yes" ]]; then
+
+    # Create a filename based on the project ID
+    FILENAME="${PROJECT_ID}_owners.txt"
+
+    echo "Saving results to $FILENAME..."
+
+    # Use a Block Redirect to write all output to the file at once
+    {
+        echo "Results for project: $PROJECT_ID"
+        echo "-----------------------------------------------"
+        if [ -z "$IAM_POLICY" ]; then
+            echo "No members found with the '$ROLE_TO_FIND' role."
+        else
+            echo "Members with the '$ROLE_TO_FIND' role in project '$PROJECT_ID':"
+            gcloud projects get-iam-policy "$PROJECT_ID" \
+                --flatten="bindings[].members" \
+                --filter="bindings.role:$ROLE_TO_FIND" \
+                --format="csv[no-heading](bindings.members)"
+        fi
+        echo
+        echo "Time of extraction: ${TIMESTAMP}"
+        echo "-----------------------------------------------"
+    } > "$FILENAME"
     
+    echo "Results succesfully saved to $FILENAME."
+else
+    echo "Results not saved to a file. Finished."
+fi
